da06cb6f1027b8891640b3751d0f1576b0d9e36c
Tim Millett <timothy.millett@cmhworks.com>
2021-07-13 18:09:07 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,13 +16,17 @@ pipeline {
                     git 'git@github.com:teej2542/Signal-Android-test.git'
 
                     try {
-                        sh "./gradlew test -x lintchecks:test --continue"
+                        sh "./gradlew test collectResults -x lintchecks:test --continue"
                     } catch (ignored) {
-                        junit '**/build/test-results/**/*.xml'
-                    } finally {
-                        sh "./gradlew collectResults"
                         sh "mv unit-tests.csv unit-tests-${env.BUILD_NUMBER}.csv"
                         archiveArtifacts artifacts: "unit-tests-${env.BUILD_NUMBER}.csv", followSymlinks: false
+                        junit '**/build/test-results/**/*.xml'
+                    } finally {
+                        echo "done!"
+//                         sh "mv unit-tests.csv unit-tests-${env.BUILD_NUMBER}.csv"
+//                         archiveArtifacts artifacts: "unit-tests-${env.BUILD_NUMBER}.csv", followSymlinks: false
+//                         junit '**/build/test-results/**/*.xml'
+                       
                     }
                 }
             }
